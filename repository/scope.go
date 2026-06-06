716df25e9f62bc42873d9b6eb074bd3da4573ae3
kamalyes <501893067@qq.com>
2026-06-06 20:31:58 +0800

    ✨ feat(repository): 增加WithScopeFilters方法先应用作用域，再追加业务过滤条件

--- repository/scope.go
+++ repository/scope.go
@@ -15,10 +15,31 @@ type ScopeHook interface {
 	ApplyScope(query *Query) *Query
 }
 
+// QueryFilterBuilder 在已有 Query 上追加业务过滤条件
+type QueryFilterBuilder func(query *Query)
+
 // WithScope 为查询添加作用域钩子
 func (q *Query) WithScope(hook ScopeHook) *Query {
 	if hook != nil {
 		return hook.ApplyScope(q)
+	}
+	return q
+}
+
+// WithScopeFilters 先应用作用域，再追加业务过滤条件
+// 当业务搜索字段包含 tenant_id/region_code/platform_id 等作用域字段时，
+// 使用该方法可以保证搜索条件只会收窄权限范围，而不会被 ScopeHook 清理掉
+func (q *Query) WithScopeFilters(hook ScopeHook, builders ...QueryFilterBuilder) *Query {
+	q.WithScope(hook)
+	return q.WithFilters(builders...)
+}
+
+// WithFilters 追加一组业务过滤构造函数
+func (q *Query) WithFilters(builders ...QueryFilterBuilder) *Query {
+	for _, builder := range builders {
+		if builder != nil {
+			builder(q)
+		}
 	}
 	return q
 }
